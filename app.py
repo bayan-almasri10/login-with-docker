ea87374895bfa8373fe1a1d5a5210ed6a7718624
Ahmed <ahmed.g@turing.com>
2025-04-13 10:56:58 +0100

    update port configuration from 5001 to 5000 in app.py and docker-compose.yml

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         print("Database tables created (if not exist).")
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
